f13ed0b3968d29053df6631883b0599baf34e750
Tulio Dias <tuliodias@gmail.com>
2025-03-17 17:49:01 -0300

    First commit

---
+++ script.js
@@ -0,0 +1,36 @@
+'use strict';
+// SOLUTION IF WE JUST WANTED TO LOAD FROM A LOCAL FILE
+// function newQuote() {
+//   // Pick a random number from apiQuotes array
+//   const quote = localQuotes[Math.floor(Math.random() * localQuotes.length)];
+//   console.log(quote);
+// }
+
+// newQuote();
+
+let apiQuotes = [];
+
+// Show New Quote
+function newQuote() {
+  // Pick a random number from apiQuotes array
+  const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+  console.log(quote);
+}
+
+// Get Quotes from API
+async function getQuotes() {
+  const apiUrl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
+
+  // Try Catch Statement: if we fail at trying to complete a fetch request we can catch an error log and do something with it
+  try {
+    const response = await fetch(apiUrl); // so this const will not be populated until we have a result from the fetch api
+    apiQuotes = await response.json();
+    console.log(apiQuotes);
+    newQuote();
+  } catch (error) {
+    alert(error);
+  }
+}
+
+// On Load
+getQuotes();
