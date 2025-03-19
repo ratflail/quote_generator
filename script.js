202cb40cc714a632712f3299f7c7998093bcb747
Tulio Dias <tuliodias@gmail.com>
2025-03-19 17:48:19 -0300

    Final Code

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 'use strict';
-// Redeploy
 
 // SOLUTION IF WE JUST WANTED TO LOAD FROM A LOCAL FILE
 // function newQuote() {
@@ -10,33 +9,87 @@
 
 // newQuote();
 
+// BEGINNING OF ACTUAL SCRIPT
+
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.querySelector('.quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.querySelector('.new-quote');
+const loader = document.getElementById('loader');
+
 let apiQuotes = [];
+
+// SHOW LOADING
+
+function loading() {
+  loader.hidden = false; // the hidden attribute is available in any html element
+  quoteContainer.hidden = true;
+}
+
+// HIDE LOADING
+
+function complete() {
+  quoteContainer.hidden = false;
+  loader.hidden = true;
+}
 
 // Show New Quote
 function newQuote() {
+  loading(); // here again because when we press the btn we are bypassing the getquotes() from reloading the page
   // Pick a random number from apiQuotes array
   const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-  //console.log(quote);
-  document.querySelector('.quote').textContent = quote.text;
-  document.getElementById('author').textContent = quote.author;
+
+  // authorText.textContent = quote.author; This was the original code but we need to account for the possibility that the object property of author gives us Null instead of the name of an author. So:
+  // CHECK IF AUHTOR FIELD IS BLANK AND REPLACE IT WITH 'UNKNOWN'
+
+  if (!quote.author) {
+    authorText.textContent = 'Unknown';
+  } else {
+    authorText.textContent = quote.author;
+  }
+
+  // CHECK CODE LENGTH TO DETERMINE STYLE
+
+  if (quote.text.length > 120) {
+    quoteText.classList.add('long-quote');
+  } else {
+    quoteText.classList.remove('long-quote');
+  }
+
+  // SET THE QUOTE AND HIDE LOADER
+
+  quoteText.textContent = quote.text;
+  complete();
 }
 
 // Get Quotes from API
 async function getQuotes() {
+  loading();
   const apiUrl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
 
   // Try Catch Statement: if we fail at trying to complete a fetch request we can catch an error log and do something with it
   try {
     const response = await fetch(apiUrl); // so this const will not be populated until we have a result from the fetch api
     apiQuotes = await response.json();
-    console.log(apiQuotes);
-    //newQuote();
+
+    newQuote();
   } catch (error) {
     alert(error);
   }
 }
 
-document.querySelector('.new-quote').addEventListener('click', newQuote);
+// TWEET A QUOTE
+function tweetQuote() {
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+
+  // Opens up a window in a new tab
+  window.open(twitterUrl, '_blank');
+}
+
+// EVENT LISTENERS
+newQuoteBtn.addEventListener('click', newQuote);
+twitterBtn.addEventListener('click', tweetQuote);
 
 // On Load
 getQuotes();
